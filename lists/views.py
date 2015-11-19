e1f214ff214dc5033d2be2329668eea6da1d69f0
Stacey Sharp <ssharpjr@gmail.com>
2015-11-19 15:18:00 -0500

    Refactored test, using home template

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render
 
 
 def home_page(request):
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    return render(request, 'home.html')
