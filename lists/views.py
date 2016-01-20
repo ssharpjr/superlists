fb553e43370d3a258a1d36ae28fb0d444e3daa55
Stacey Sharp <ssharpjr@gmail.com>
2016-01-20 10:05:27 -0500

    Custom form save, Refactored views.

--- lists/views.py
+++ lists/views.py
@@ -11,11 +11,10 @@ def home_page(request):
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
     form = ItemForm()
-
     if request.method == 'POST':
         form = ItemForm(data=request.POST)
         if form.is_valid():
-            Item.objects.create(text=request.POST['text'], list=list_)
+            form.save(for_list=list_)
             return redirect(list_)
     return render(request, 'list.html', {'list': list_, "form": form})
 
